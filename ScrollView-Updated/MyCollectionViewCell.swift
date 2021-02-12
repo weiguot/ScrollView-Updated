10f0217f60fa35a5656163a5b94a8573f4f5463b
Weiguo Tao <weiguotao12@gmail.com>
2021-02-11 21:54:10 -0500

    First Commit

---
+++ ScrollView-Updated/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+
+
+//
+//  MyCollectionViewCell.swift
+//  ScrollView
+//
+//  Created by Juan carlos De la parra on 11/02/21.
+//
+
+import UIKit
+
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UIImageView!
+    // @IBOutlet weak var myLabel: UILabel!
+}
--- ScrollView-Updated/ViewController.swift
+++ ScrollView-Updated/ViewController.swift
@@ -1,19 +1,50 @@
 //
 //  ViewController.swift
-//  ScrollView-Updated
+//  ScrollView
 //
-//  Created by Field Employee on 2/11/21.
+//  Created by Juan carlos De la parra on 11/02/21.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    let mycollectionViewCell = MyCollectionViewCell()
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+   // var items: [UIImage] = []    //var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+   var items = Array(repeating: UIImage(named: "cat.jpg"), count: 100)
+    
+   
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       // print(items[1])
+        return self.items.count
+        
     }
-
-
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        //cell.myLabel.text = self.items[indexPath.row] // The row value is the same as the index of the desired text within the array.
+       // cell.myLabel.image = self.items[(UIImage(named: "cat.jpg"))]
+        cell.myLabel.image = self.items[indexPath.row]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
 }
+
+
 
